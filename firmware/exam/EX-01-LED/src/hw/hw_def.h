d4cae357ccc94c41dc80da82f65c74e3c25b968a
Hancheol Cho <chcbaram@gmail.com>
2024-02-17 00:32:52 +0900

    LED 예제 추가

---
+++ firmware/exam/EX-01-LED/src/hw/hw_def.h
@@ -0,0 +1,27 @@
+#ifndef HW_DEF_H_
+#define HW_DEF_H_
+
+
+
+#include "bsp.h"
+
+
+#define _DEF_FIRMWATRE_VERSION    "V240214R1"
+#define _DEF_BOARD_NAME           "STM32MP135-FW"
+
+
+
+#define _USE_HW_LED
+#define      HW_LED_MAX_CH          2
+#define      HW_LED_CH_LED_R        _DEF_LED1
+#define      HW_LED_CH_LED_B        _DEF_LED2
+
+
+
+//-- USE CLI
+//
+#define _USE_CLI_HW_LED             1
+
+
+
+#endif
